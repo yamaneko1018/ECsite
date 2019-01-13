52b86bd912e43f8ed3d08b9c1bd23ae1ea8d55f8
vagrant <vagrant@localhost.localdomain>
2019-01-13 21:27:28 +0900

    ページング

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,6 +2,6 @@ class HistoriesController < ApplicationController
  before_action :authenticate_user!
   def index
    @histories = History.where(user_id: current_user.id)
-  
+   @histories = History.page(params[:page]).per(10)  
   end
 end
