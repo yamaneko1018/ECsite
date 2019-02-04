d7adc9831c0fc41caa3de242df7423467c14b36e
vagrant <vagrant@localhost.localdomain>
2019-02-04 17:40:40 +0000

    検索機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @items = Item.all
-    @q = Item.search(params[:q])
-    @items = @q.result(distinct: true)
+    if params[:stock]
+     @q = Item.all.search(params[:q])
+     @items = @q.result
+    else
+     @items = Item.where.not(stock:  0)
+     @q = @items.search(params[:q])
+     @items = @q.result
+    end
   end
 end
