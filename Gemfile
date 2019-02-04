3f3da7906eb501bbec2fecc73a36e127ce7ae6eb
vagrant <vagrant@localhost.localdomain>
2019-02-04 13:17:37 +0000

    検索機能実装

--- Gemfile
+++ Gemfile
@@ -66,4 +66,4 @@ gem 'bootstrap', '~> 4.2.1'
 gem 'jquery-rails'
 gem 'devise-i18n'
 gem 'devise-i18n-views'
-
+gem 'ransack'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!
   def index
     @items = Item.all
+    @q = Item.search(params[:q])
+    @items = @q.result(distinct: true)
   end
 end
