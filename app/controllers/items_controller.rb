e3700fdb69e6f93af6c7ffc87185bbd16e8ee84e
vagrant <vagrant@localhost.localdomain>
2019-02-04 19:44:47 +0000

    検索機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,28 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
+
   def index
-    if params[:stock]
-     @q = Item.all.search(params[:q])
-     @items = @q.result
-    else
-     @items = Item.where.not(stock:  0)
-     @q = @items.search(params[:q])
-     @items = @q.result
-    end
+   if params[:stock] && params[:flag]
+    @q = Item.all.search(params[:q])
+    @items = @q.result
+  elsif  params[:stock]
+    @items = Item.where(flag: 0)
+    @q = @items.ransack(params[:q])
+    @items = @q.result
+  elsif params[:flag]
+    @items = Item.where.not(stock:  0)
+    @q = @items.ransack(params[:q])
+    @items = @q.result
+  else
+   @items = Item.where.not(stock:  0, flag: 1)
+   @q = @items.ransack(params[:q])
+   @items = @q.result
   end
+end
+
+
+
+
+
+
 end
