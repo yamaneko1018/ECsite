28992a5b600bcbb1b25db41a18fdebfc0b7692f9
vagrant <vagrant@localhost.localdomain>
2019-02-22 15:29:01 +0900

    購入時刻降順設定

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,6 +2,7 @@ class HistoriesController < ApplicationController
  before_action :authenticate_user!
   def index
    @search = History.ransack(params[:q])
+   @search.sorts = 'date desc' if @search.sorts.empty?
    @histories = @search.result.page(params[:page]).per(10)  
  #  @histories = History.order('quantity,total_amount,date')  
 end
